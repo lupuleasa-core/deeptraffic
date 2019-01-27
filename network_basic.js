b7ef4d82b524944367e81ba5718f3136c32f7df6
Lup <5671120+lupuleasa-core@users.noreply.github.com>
2019-01-27 20:56:06 +0200

    Iteration 1
    First attempt.
    Tweaked look space, 361 spaces considered.
    Changed the middle FC layer to 64 neurons.
    Pumped training iterations.
    Various hyperparam tweaks.
    Training time: ~5 mins
    Avg speed: 65km/h

--- network_basic.js
+++ network_basic.js
@@ -1,17 +1,18 @@
+
 //<![CDATA[
 
 // a few things don't have var in front of them - they update already existing variables the game needs
-lanesSide = 1;
-patchesAhead = 10;
-patchesBehind = 0;
-trainIterations = 30000;
+lanesSide = 3;
+patchesAhead = 6;
+patchesBehind = 2;
+trainIterations = 35000;
 
 // the number of other autonomous vehicles controlled by your network
-otherAgents = 0; // max of 10
+otherAgents = 5; // max of 10
 
 var num_inputs = (lanesSide * 2 + 1) * (patchesAhead + patchesBehind);
 var num_actions = 5;
-var temporal_window = 0;
+var temporal_window = 5;
 var network_size = num_inputs * temporal_window + num_actions * temporal_window + num_inputs;
 
 var layer_defs = [];
@@ -23,7 +24,7 @@ var layer_defs = [];
 });
 layer_defs.push({
     type: 'fc',
-    num_neurons: 10,
+    num_neurons: 64,
     activation: 'relu'
 });
 layer_defs.push({
@@ -33,20 +34,20 @@ layer_defs.push({
 
 var tdtrainer_options = {
     learning_rate: 0.001,
-    momentum: 0.0,
-    batch_size: 64,
+    momentum: 0.1,
+    batch_size: 16,
     l2_decay: 0.01
 };
 
 var opt = {};
 opt.temporal_window = temporal_window;
-opt.experience_size = 3000;
+opt.experience_size = 4000;
 opt.start_learn_threshold = 500;
-opt.gamma = 0.5;
+opt.gamma = 0.3;
 opt.learning_steps_total = 10000;
-opt.learning_steps_burnin = 1000;
-opt.epsilon_min = 0.0;
-opt.epsilon_test_time = 0.0;
+opt.learning_steps_burnin = 2500;
+opt.epsilon_min = 0.01;
+opt.epsilon_test_time = 0.01;
 opt.layer_defs = layer_defs;
 opt.tdtrainer_options = tdtrainer_options;
 
